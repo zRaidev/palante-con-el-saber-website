aa17cc68fabe7406280788794b9d170d91efe421
macRaidev <isaac14e2007@gmail.com>
2026-04-10 21:33:52 -0500

    Feat: Vercel SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -33,6 +34,7 @@ export default function RootLayout({
       <body className="font-[system-ui] antialiased">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
