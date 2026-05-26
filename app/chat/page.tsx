9ccd9a45e2d3eb69cc124f8cce4ce8bd31ae65bf
zRaidev <isaac14e2007@gmail.com>
2026-05-25 21:30:23 -0500

    Feat: Add metadata for ChatPage component to enhance SEO and social sharing

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,4 +1,20 @@
 import FrenChat from '@/components/Chat/FrenChat';
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "El Compa AI | Pa'lante Con El Saber",
+  description: "El Compa AI es un chatbot desarrollado por el equipo de Pa'lante con el Saber que ayuda a jóvenes panameños a elegir su bachillerato. El chatbot utiliza inteligencia artificial para proporcionar información personalizada sobre las diferentes opciones de bachillerato disponibles en Panamá, ayudando a los estudiantes a tomar decisiones informadas sobre su futuro académico.",
+  keywords: ["equipo Pa'lante Con El Saber", "jóvenes panameños", "Laboratorio Acción Ciudadana", "IA", "inteligencia artificial", "chatbot", "bachillerato", "educación en Panamá", "futuro académico"],
+  openGraph: {
+    title: "El Compa AI | Pa'lante Con El Saber",
+    description: "El Compa AI es un chatbot desarrollado por el equipo de Pa'lante con el Saber que ayuda a jóvenes panameños a elegir su bachillerato. El chatbot utiliza inteligencia artificial para proporcionar información personalizada sobre las diferentes opciones de bachillerato disponibles en Panamá, ayudando a los estudiantes a tomar decisiones informadas sobre su futuro académico.",
+    url: "https://palanteconelsaber.site/chat",
+    siteName: "Pa'lante Con El Saber",
+    images: [{ url: "https://palanteconelsaber.site/logo-white.png" }],
+    locale: "es_PA",
+    type: "website",
+  },
+}
 
 export default function ChatPage() {
   return (
