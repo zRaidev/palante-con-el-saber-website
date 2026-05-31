a770792aa5c2ad737f17a70e75ba40b614666769
macRaidev <isaac14e2007@gmail.com>
2026-05-31 05:42:37 -0500

    Feat: Update metadata for ChatPage component to reflect correct branding

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,12 +2,12 @@ import FrenChat from '@/components/Chat/FrenChat';
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: "El Compa AI | Pa'lante Con El Saber",
-  description: "El Compa AI es un chatbot desarrollado por el equipo de Pa'lante con el Saber que ayuda a jóvenes panameños a elegir su bachillerato. El chatbot utiliza inteligencia artificial para proporcionar información personalizada sobre las diferentes opciones de bachillerato disponibles en Panamá, ayudando a los estudiantes a tomar decisiones informadas sobre su futuro académico.",
+  title: "El Fren AI | Pa'lante Con El Saber",
+  description: "El Fren AI es un chatbot desarrollado por el equipo de Pa'lante con el Saber que ayuda a jóvenes panameños a elegir su bachillerato. El chatbot utiliza inteligencia artificial para proporcionar información personalizada sobre las diferentes opciones de bachillerato disponibles en Panamá, ayudando a los estudiantes a tomar decisiones informadas sobre su futuro académico.",
   keywords: ["equipo Pa'lante Con El Saber", "jóvenes panameños", "Laboratorio Acción Ciudadana", "IA", "inteligencia artificial", "chatbot", "bachillerato", "educación en Panamá", "futuro académico"],
   openGraph: {
-    title: "El Compa AI | Pa'lante Con El Saber",
-    description: "El Compa AI es un chatbot desarrollado por el equipo de Pa'lante con el Saber que ayuda a jóvenes panameños a elegir su bachillerato. El chatbot utiliza inteligencia artificial para proporcionar información personalizada sobre las diferentes opciones de bachillerato disponibles en Panamá, ayudando a los estudiantes a tomar decisiones informadas sobre su futuro académico.",
+    title: "El Fren AI | Pa'lante Con El Saber",
+    description: "El Fren AI es un chatbot desarrollado por el equipo de Pa'lante con el Saber que ayuda a jóvenes panameños a elegir su bachillerato. El chatbot utiliza inteligencia artificial para proporcionar información personalizada sobre las diferentes opciones de bachillerato disponibles en Panamá, ayudando a los estudiantes a tomar decisiones informadas sobre su futuro académico.",
     url: "https://palanteconelsaber.site/chat",
     siteName: "Pa'lante Con El Saber",
     images: [{ url: "https://palanteconelsaber.site/logo-white.png" }],
